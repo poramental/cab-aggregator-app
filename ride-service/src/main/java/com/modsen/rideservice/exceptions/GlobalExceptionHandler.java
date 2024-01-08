de42c0ceb0284f8f6e59107dd2fca30f7fd34476
Alesad <74681470+poramental@users.noreply.github.com>
2024-01-08 12:33:14 +0300

    add: basic exceptions and global exception handler

---
+++ ride-service/src/main/java/com/modsen/rideservice/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.modsen.rideservice.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<AppError> rideIsPresentExceptionHandler(RideIsPresentException e){
+        return new ResponseEntity<>(new AppError(e.getMessage()), HttpStatus.CONFLICT);
+    }
+
+    public ResponseEntity<AppError> rideNotFoundExceptionHandler(RideNotFoundException e){
+        return new ResponseEntity<>(new AppError(e.getMessage()),HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+    }
+
+}
---
+++ ride-service/src/main/java/com/modsen/rideservice/exceptions/RideIsPresentException.java
@@ -0,0 +1,7 @@
+package com.modsen.rideservice.exceptions;
+
+public class RideIsPresentException extends Exception{
+    public RideIsPresentException(String m) {
+        super(m);
+    }
+}
