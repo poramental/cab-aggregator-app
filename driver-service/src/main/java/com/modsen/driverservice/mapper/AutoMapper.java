ebad49e10416fcc13c4fc494b6fc9abb9f0df754
poramental <poramental@mail.ru>
2024-01-27 14:10:39 +0300

    refactor: added ModelMapperConfig

--- driver-service/src/main/java/com/modsen/driverservice/mapper/AutoMapper.java
+++ driver-service/src/main/java/com/modsen/driverservice/mapper/AutoMapper.java
@@ -2,23 +2,22 @@ package com.modsen.driverservice.mapper;
 
 import com.modsen.driverservice.dto.AutoDto;
 import com.modsen.driverservice.entity.Auto;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class AutoMapper {
+
     private final ModelMapper mapper;
 
-    public AutoMapper(){
-        this.mapper = new ModelMapper();
+    public AutoDto entityToDto(Auto auto) {
+        return mapper.map(auto, AutoDto.class);
     }
 
-    public AutoDto entityToDto(Auto auto){
-        return mapper.map(auto,AutoDto.class);
-    }
-
-    public Auto dtoToEntity(AutoDto autoDto){
-        return mapper.map(autoDto,Auto.class);
+    public Auto dtoToEntity(AutoDto autoDto) {
+        return mapper.map(autoDto, Auto.class);
     }
 
 }
--- driver-service/src/main/java/com/modsen/driverservice/mapper/DriverMapper.java
+++ driver-service/src/main/java/com/modsen/driverservice/mapper/DriverMapper.java
@@ -3,28 +3,24 @@ package com.modsen.driverservice.mapper;
 import com.modsen.driverservice.dto.DriverRequest;
 import com.modsen.driverservice.dto.DriverResponse;
 import com.modsen.driverservice.entity.Driver;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
-import java.util.stream.Collectors;
 
 
 @Component
+@RequiredArgsConstructor
 public class DriverMapper {
 
     private final ModelMapper mapper;
     private final AutoMapper autoMapper;
 
-    public DriverMapper(){
-        this.mapper = new ModelMapper();
-        this.autoMapper = new AutoMapper();
-    }
-
     public DriverResponse entityToRespDto(Driver driver){
         DriverResponse driverDto = mapper.map(driver, DriverResponse.class);
         return driverDto.getAutos().isEmpty() ? driverDto :
                 driverDto.setAutos(driver.getAutos().stream()
-                        .map(autoMapper::entityToDto).collect(Collectors.toList()));
+                        .map(autoMapper::entityToDto).toList());
     }
 
     public Driver reqDtoToEntity(DriverRequest driverDto){
