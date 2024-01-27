c1447d3b79a128f05ad5b29f6de42ae740447aa6
poramental <poramental@mail.ru>
2024-01-27 14:22:30 +0300

    refactor

--- driver-service/src/main/java/com/modsen/driverservice/controller/DriverController.java
+++ driver-service/src/main/java/com/modsen/driverservice/controller/DriverController.java
@@ -19,66 +19,58 @@ public class DriverController {
 
 
     @GetMapping
-    public ResponseEntity<ListDriverResponse> getAll(){
+    public ResponseEntity<ListDriverResponse> getAll() {
         return ResponseEntity.ok(driverService.getAll());
     }
 
     @PostMapping
-    public ResponseEntity<DriverResponse> add(@RequestBody @Valid DriverRequest driverDto)
-    {
+    public ResponseEntity<DriverResponse> add(@RequestBody @Valid DriverRequest driverDto) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(driverService.add(driverDto));
     }
 
     @DeleteMapping("/{driverId}")
-    public ResponseEntity<DriverResponse> deleteById(@PathVariable Long driverId)
-    {
+    public ResponseEntity<DriverResponse> deleteById(@PathVariable Long driverId) {
         return ResponseEntity
                 .status(HttpStatus.NO_CONTENT)
                 .body(driverService.deleteById(driverId));
     }
 
     @GetMapping("/{driverId}")
-    public ResponseEntity<DriverResponse> getById(@PathVariable Long driverId)
-    {
+    public ResponseEntity<DriverResponse> getById(@PathVariable Long driverId) {
         return ResponseEntity.ok(driverService.getById(driverId));
     }
 
     @PutMapping("/{driverId}")
     public ResponseEntity<DriverResponse> update(@PathVariable Long driverId,
-                                                 @RequestBody @Valid DriverRequest driverDto)
-    {
-        return ResponseEntity.ok(driverService.update(driverId,driverDto));
+                                                 @RequestBody @Valid DriverRequest driverDto) {
+        return ResponseEntity.ok(driverService.update(driverId, driverDto));
     }
 
     @GetMapping("/page")
     public ResponseEntity<DriverPageResponse> getPage(@RequestParam int page,
                                                       @RequestParam int size,
-                                                      @RequestParam String orderBy)
-    {
-        return ResponseEntity.ok(driverService.getDriversPage(page,size,orderBy));
+                                                      @RequestParam String orderBy) {
+        return ResponseEntity.ok(driverService.getDriversPage(page, size, orderBy));
     }
 
     @PostMapping("{driverId}/auto")
     public ResponseEntity<DriverResponse> setAutoById(@PathVariable Long driverId,
-                                                      @RequestBody AutoDto autoDto)
-    {
-        return ResponseEntity.ok(driverService.setAutoById(driverId,autoDto));
+                                                      @RequestBody AutoDto autoDto) {
+        return ResponseEntity.ok(driverService.setAutoById(driverId, autoDto));
     }
 
     @PatchMapping("/{driverId}/rating")
     public ResponseEntity<DriverResponse> addRating(@PathVariable Long driverId,
-                                                    @RequestParam int rating)
-    {
-        return ResponseEntity.ok(driverService.addRatingById(driverId,rating));
+                                                    @RequestParam int rating) {
+        return ResponseEntity.ok(driverService.addRatingById(driverId, rating));
     }
 
     @PutMapping("/{driverId}/auto")
-    public  ResponseEntity<DriverResponse> replaceAutoById(@PathVariable Long driverId,
-                                                           @RequestBody @Valid AutoDto autoDto)
-    {
-        return ResponseEntity.ok(driverService.replaceAutoById(driverId,autoDto));
+    public ResponseEntity<DriverResponse> replaceAutoById(@PathVariable Long driverId,
+                                                          @RequestBody @Valid AutoDto autoDto) {
+        return ResponseEntity.ok(driverService.replaceAutoById(driverId, autoDto));
     }
 
 }
