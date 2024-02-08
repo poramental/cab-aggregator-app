5045e00bd9c1a02afb0eb5ed2632c7c509a16511
poramental <poramental@mail.ru>
2024-02-08 18:15:35 +0300

    refactor

--- payment-service/src/main/java/com/modsen/paymentservice/dto/CardRequest.java
+++ payment-service/src/main/java/com/modsen/paymentservice/dto/CardRequest.java
@@ -1,5 +1,6 @@
 package com.modsen.paymentservice.dto;
 
+import com.modsen.paymentservice.util.RegexpValidation;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -10,7 +11,7 @@ import lombok.Setter;
 @Setter
 public class CardRequest {
     @NotBlank(message = "card.number.not.blank")
-    @Pattern(regexp = "\\d{16}", message = "card.number.pattern")
+    @Pattern(regexp = RegexpValidation.cardExp, message = "card.number.pattern")
     private String cardNumber;
     @NotNull(message = "exp.month.not.blank")
     private Integer expM;
