9efc94b6b69363e39b24f8f2e2db9eb4efd1efb4
poramental <poramental@mail.ru>
2024-02-12 15:51:36 +0300

    payment-service-communications

--- payment-service/src/main/java/com/modsen/paymentservice/exceptions/CustomerCreatingException.java
+++
@@ -1,7 +0,0 @@
-package com.modsen.paymentservice.exceptions;
-
-public class CustomerCreatingException extends RuntimeException {
-    public CustomerCreatingException(String ex) {
-        super(ex);
-    }
-}
