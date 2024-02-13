3c78463ca6248bd877fa5c612ceea11ee087b920
Alesad <74681470+poramental@users.noreply.github.com>
2024-02-13 20:29:19 +0300

    refactor

--- passenger-service/src/main/java/com/modsen/passengerservice/controller/PassengerController.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/controller/PassengerController.java
@@ -9,7 +9,6 @@ import com.modsen.passengerservice.service.PassengerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
