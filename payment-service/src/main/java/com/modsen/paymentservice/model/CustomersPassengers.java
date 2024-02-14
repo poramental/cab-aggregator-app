17f9c2a493c520d95336a089eeb77cf6584c7880
poramental <poramental@mail.ru>
2024-02-14 21:16:03 +0300

    refactor

--- payment-service/src/main/java/com/modsen/paymentservice/model/CustomersPassengers.java
+++ payment-service/src/main/java/com/modsen/paymentservice/model/CustomersPassengers.java
@@ -3,7 +3,11 @@ package com.modsen.paymentservice.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Getter
 @Setter
