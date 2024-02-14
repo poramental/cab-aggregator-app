508d16eb014d329491c1b7063647f1a9183851e2
poramental <poramental@mail.ru>
2024-02-14 23:32:23 +0300

    refactor: renamed ValidationRegExp -> RegExpConstants

--- payment-service/src/main/java/com/modsen/paymentservice/util/RegExpConstants.java
+++ payment-service/src/main/java/com/modsen/paymentservice/util/RegExpConstants.java
@@ -3,6 +3,6 @@ package com.modsen.paymentservice.util;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
-public class ValidationRegExp {
+public class RegExpConstants {
     public final String cardExp = "\\d{16}";
 }
