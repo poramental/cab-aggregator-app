5c7756e4b93304bfab7d4d7342cacd0cde32092f
poramental <poramental@mail.ru>
2024-02-15 20:59:32 +0300

    add: api-gateway, routes to passenger-service

--- passenger-service/src/main/java/com/modsen/passengerservice/PassengerServiceApplication.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/PassengerServiceApplication.java
@@ -2,10 +2,12 @@ package com.modsen.passengerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PassengerServiceApplication {
 
     public static void main(String[] args) {
--- passenger-service/src/main/java/com/modsen/passengerservice/controller/GlobalExceptionHandler.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/controller/GlobalExceptionHandler.java
@@ -5,19 +5,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
 
-    @ExceptionHandler({PassengerNotFoundException.class, PassengerAlreadyExistException.class})
+    @ExceptionHandler(PassengerNotFoundException.class)
     public ResponseEntity<AppError> notFoundException(PassengerNotFoundException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new AppError(ex.getMessage()));
+    }
+
+    @ExceptionHandler(PassengerAlreadyExistException.class)
+    public ResponseEntity<AppError> conflict(PassengerAlreadyExistException ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(new AppError(ex.getMessage()));
     }
 
