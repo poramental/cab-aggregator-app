17103fc3f73862648d3b7d2ad5940218fb115f19
poramental <poramental@mail.ru>
2024-02-21 13:45:49 +0300

    refactor: fixed import lombok.*

--- passenger-service/src/main/java/com/modsen/passengerservice/dto/ListPassengerResponse.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/dto/ListPassengerResponse.java
@@ -1,6 +1,13 @@
 package com.modsen.passengerservice.dto;
 
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
