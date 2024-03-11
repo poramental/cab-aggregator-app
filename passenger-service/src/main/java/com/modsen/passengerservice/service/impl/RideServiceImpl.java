853a94388f24aeb934fa771c07f644b66fe55358
Alesad <74681470+poramental@users.noreply.github.com>
2024-03-11 23:27:04 +0300

    add: log to passenger-service

--- passenger-service/src/main/java/com/modsen/passengerservice/service/impl/RideServiceImpl.java
+++ passenger-service/src/main/java/com/modsen/passengerservice/service/impl/RideServiceImpl.java
@@ -8,18 +8,21 @@ import com.modsen.passengerservice.util.ExceptionMessages;
 import feign.RetryableException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
+import static com.modsen.passengerservice.util.LogMessages.GET_RIDE_BY_ID_METHOD_CALL;
 import java.util.UUID;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class RideServiceImpl implements RideService {
 
     private final RideFeignClient rideFeignClient;
 
     @CircuitBreaker(name = "ride", fallbackMethod = "fallbackMethod")
-    public RideResponse getRideById(UUID id){
+    public RideResponse getRideById(UUID id) {
+        log.info(GET_RIDE_BY_ID_METHOD_CALL, id);
         return rideFeignClient.getRideById(id);
     }
 
---
+++ passenger-service/src/main/java/com/modsen/passengerservice/util/LogMessages.java
@@ -0,0 +1,13 @@
+package com.modsen.passengerservice.util;
+
+public class LogMessages {
+    public static final String GET_ALL_CONTROLLER_METHOD_CALL = "User with ip: {} call method getAllPassengers()";
+    public static final String ADD_CONTROLLER_METHOD_CALL = "User with ip: {} call method addPassenger()";
+    public static final String DELETE_CONTROLLER_METHOD_CALL = "User with ip: {} call method deletePassenger()";
+    public static final String GET_BY_ID_CONTROLLER_METHOD_CALL = "User with ip: {} call method getPassengerById()";
+    public static final String UPDATE_BY_ID_CONTROLLER_METHOD_CALL = "User with ip: {} call method updatePassengerById()";
+    public static final String GET_PAGE_CONTROLLER_METHOD_CALL = "User with ip: {} call method getPageOfPassengers()";
+    public static final String ADD_RATING_CONTROLLER_METHOD_CALL = "User with ip: {} call method addRatingToPassenger()";
+    public static final String AN_EXCEPTION_OCCURRED =  "An exception occurred: {}";
+    public static final String GET_RIDE_BY_ID_METHOD_CALL = "Sending request to ride-service to get ride with id: {}";
+}
