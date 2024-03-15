e562e9ef94ff16d4b3c0bb1c6797390f1af3074a
Alesad <74681470+poramental@users.noreply.github.com>
2024-03-15 05:36:18 +0300

    add: security to ride-service, payment-service, driver-service

---
+++ driver-service/src/main/java/com/modsen/driverservice/security/JwtAuthConverter.java
@@ -0,0 +1,62 @@
+package com.modsen.driverservice.security;
+
+
+import com.modsen.driverservice.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.modsen.driverservice.util.SecurityUtil.RESOURCES_ACCESS;
+import static com.modsen.driverservice.util.SecurityUtil.ROLES;
+
+@RequiredArgsConstructor
+@Component
+public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+    private final JwtAuthConverterProperties properties;
+
+    public AbstractAuthenticationToken convert(Jwt jwt) {
+        Collection<GrantedAuthority> authorities = Stream.concat(
+                jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                extractResourceRoles(jwt).stream()).collect(Collectors.toSet());
+        User user = SecurityUtil.extractUserInfo(jwt);
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                user,
+                null,
+                authorities
+        );
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        return authToken;
+    }
+
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess = jwt.getClaim(RESOURCES_ACCESS);
+        Map<String, Object> resource;
+        Collection<String> resourceRoles;
+
+        if (resourceAccess == null
+                || (resource = (Map<String, Object>) resourceAccess.get(properties.getResourceId())) == null
+                || (resourceRoles = (Collection<String>) resource.get(ROLES)) == null) {
+            return Set.of();
+        }
+
+        return resourceRoles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+    }
+}
